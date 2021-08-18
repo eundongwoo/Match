c69336ee6f87b2e463d05a6f11ff697a62718e3d
ramdam427 <useroff@naver.com>
2021-08-18 14:05:13 +0900

    articleDao랑 ArticleContentDao를 합쳤습니다. table에 content또한 not null처리했는데 그부분 에러처리는 아직 안했습니다.

--- Match/src/main/java/jdbc/matchSql.sql
+++ Match/src/main/java/jdbc/matchSql.sql
@@ -8,3 +8,24 @@ create table member(
 	password varchar(50) not null,
 	tel varchar(50) not null
 );
+
+drop table article;
+
+create table article(
+	article_no int primary key,
+	writer_id varchar(50) not null,
+	writer_name varchar(50) not null,
+	title varchar(225) not null,
+	regdate timestamp not null,
+	moddate timestamp not null,
+	content long not null,
+	read_cnt int);
+//article에 content가 추가되었음.
+
+select * from article;
+
+grant create sequence to hr;
+
+//article_no 글번호 증가 num_seq
+create sequence num_seq
+increment by 1 start with 1;
